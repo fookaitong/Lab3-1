450c6b61c960e0c8eab78887eb494aa0b5cdbeeb
user <user@FD11>
2020-03-17 11:38:22 +0800

    homework

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello,World!");
+		String lyrics = "Let \"her\" go! Let \"her\" go! Can\'t hold \"her\" back anymore";
+		System.out.print(lyrics);
 	}
 
 }
